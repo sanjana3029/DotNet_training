0914eabed33e9a063bba415b5ad732cf054803db
Sanjana Gupta <sanjanagu@infinite.com>
2024-03-06 10:48:05 +0530

    assessment

---
+++ Assessment/Assessment_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Enter details for Undergraduate:");
+            //Console.Write("Name: ");
+            //string undergradName = Console.ReadLine();
+            //Console.Write("Student ID: ");
+            //int undergradId = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Grade: ");
+            //double undergradGrade = Convert.ToDouble(Console.ReadLine());
+
+            //Undergraduate undergrade = new Undergraduate(undergradName, undergradId, undergradGrade);
+               
+            
+
+            //Console.WriteLine($"{undergradName} passed: {undergrade.IsPassed(undergradGrade)}");
+
+            //// Create a Graduate student
+            //Console.WriteLine("\nEnter details for Graduate:");
+            //Console.Write("Name: ");
+            //string gradName = Console.ReadLine();
+            //Console.Write("Student ID: ");
+            //int gradId = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Grade: ");
+            //double gradGrade = Convert.ToDouble(Console.ReadLine());
+
+            //Graduate grad = new Graduate(gradName, gradId, gradGrade);
+
+            //Console.WriteLine($"{gradName} passed: {grad.IsPassed(gradGrade)}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Assessment/Assessment_2/Student_Grade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_2
+{
+     abstract class Student_Grade
+    {
+
+        protected string Name;
+        protected int StudentId;
+        protected double Grade;
+
+        public Student_Grade(string name, int stId, double grade)
+        {
+            Name = name;
+            StudentId = stId;
+            Grade = grade;
+        }
+
+        public abstract bool IsPassed(double grade);
+    }
+
+    class Undergraduate: Student_Grade
+    {
+        public Undergraduate(string name, int stId, double grade) : base(name, stId, grade)
+        {
+        }
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+    class Graduate : Student_Grade
+    {
+        public Graduate(string name, int stId, double grade) : base(name, stId, grade)
+        {}
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+}
