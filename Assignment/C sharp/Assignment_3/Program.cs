b76dcf19a3a62f23f155444d7edfa895a6c21e7c
Sanjana Gupta <sanjanagu@infinite.com>
2024-03-07 11:18:41 +0530

    clean

---
+++ Assignment/C sharp/Assignment_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Doctor d = new Doctor();
+            //d.Doctorinfo(123, "Simran", 500.50);
+            //d.Displayinfo();
+
+            Schlorship sch = new Schlorship();
+
+            Console.Write("Enter the Marks: ");
+            double marks = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Fees: ");
+            double fees = double.Parse(Console.ReadLine());
+
+            double amount = sch.Merit(marks, fees);
+            Console.WriteLine($"schlorship amount: {amount}");
+            Console.Read();
+
+        }
+    }
+}
---
+++ Assignment/C sharp/Assignment_3/Schlorship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class Schlorship
+    {
+        public double Merit(double marks, double fees)
+        {
+            double schlorAmount = 0;
+
+            if (marks >= 70 && marks <= 80) 
+            {
+                schlorAmount = 0.2 * fees;
+            }
+
+            else if(marks > 80 && marks <= 90)
+            {
+                schlorAmount = 0.3 * fees;
+            }
+            else if(marks >90)
+            {
+                schlorAmount = 0.5 * fees;
+            }
+
+            return schlorAmount;
+
+        }
+    }
+}
